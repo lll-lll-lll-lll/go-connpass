6a4ac0f48f1d8d1250056bb2f4b81120edc5d5a1
ShunpeIII <63499912+lll-lll-lll-lll@users.noreply.github.com>
2024-05-05 00:14:15 +0000

    fix

---
+++ example/simple/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+
+	"github.com/lll-lll-lll-lll/go-connpass/connpass"
+)
+
+func main() {
+	client := &connpass.Client{}
+	q := map[string]string{"nickname": "your connpass user name"}
+	res, _ := client.Do(context.Background(), connpass.Query(q), connpass.URLV1())
+	defer res.Body.Close()
+
+	var cRes connpass.Response
+	body, _ := io.ReadAll(res.Body)
+	if err := json.Unmarshal(body, &cRes); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(cRes)
+}
